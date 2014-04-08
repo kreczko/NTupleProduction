ea38cc96794c596c70fd901ef09dfa0cf3d1c168
Luke Kreczko <lkreczko@gmail.com>
2014-04-08 12:03:34 +0100

    added lepton veto for HT jets & enabled fine-binned histograms

--- plugins/UnfoldingAnalyser.cc
+++ plugins/UnfoldingAnalyser.cc
@@ -19,7 +19,7 @@ using namespace std;
 // one has to use this thing.
 #include "TString.h"
 
-UnfoldingAnalyser::UnfoldingAnalyser(const edm::ParameterSet& iConfig) :		
+UnfoldingAnalyser::UnfoldingAnalyser(const edm::ParameterSet& iConfig) :
 		pu_weight_input_(iConfig.getParameter < edm::InputTag > ("pu_weight_input")), //
 		b_tag_weight_input(iConfig.getParameter < edm::InputTag > ("b_tag_weight_input")), //
 		gen_part_input_(iConfig.getParameter < edm::InputTag > ("gen_part_input")), //
@@ -49,8 +49,8 @@ UnfoldingAnalyser::UnfoldingAnalyser(const edm::ParameterSet& iConfig) :
 		fake_(), //
 		contamination_in_gen_variable_(), //
 		contamination_in_reco_variable_(), //
-//		response_(), //
-//		response_without_fakes_(), //
+		response_(), //
+		response_without_fakes_(), //
 		//
 		truth_asym_bins_(), //
 		measured_asym_bins_(), //
@@ -98,32 +98,32 @@ void UnfoldingAnalyser::beginJob() {
 	}
 	//cout << "This is not how you debug" << endl;
 //histograms with 1 GeV binning
- 	truth_ =
- 			fs->make < TH1F
- 					> ("truth", TString("True distribution;GEN(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
- 	measured_ =
- 			fs->make < TH1F
- 					> ("measured", TString("Measured distribution;RECO(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
- 	fake_ =
- 			fs->make < TH1F
- 					> ("fake", TString("background distribution;RECO(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
- 	contamination_in_gen_variable_ =
- 			fs->make < TH1F
- 					> ("contamination_inGenMET", TString(
- 							"background distribution;GEN(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
- 	contamination_in_reco_variable_ =
- 			fs->make < TH1F
- 					> ("contamination_inRecoMET", TString(
- 							"background distribution;RECO(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
+	truth_ =
+			fs->make < TH1F
+					> ("truth", TString("True distribution;GEN(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
+	measured_ =
+			fs->make < TH1F
+					> ("measured", TString("Measured distribution;RECO(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
+	fake_ =
+			fs->make < TH1F
+					> ("fake", TString("background distribution;RECO(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
+	contamination_in_gen_variable_ =
+			fs->make < TH1F
+					> ("contamination_inGenMET", TString(
+							"background distribution;GEN(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
+	contamination_in_reco_variable_ =
+			fs->make < TH1F
+					> ("contamination_inRecoMET", TString(
+							"background distribution;RECO(" + variable_under_analysis_ + ");# Events"), variable_n_bins_, variable_min_, variable_max_);
 
-// 	response_ =
-// 			fs->make < TH2F
-// 					> ("response", TString(
-// 							"response;RECO(" + variable_under_analysis_ + ");GEN(" + variable_under_analysis_ + ")"), variable_n_bins_, variable_min_, variable_max_, variable_n_bins_, variable_min_, variable_max_);
-// 	response_without_fakes_ =
-// 			fs->make < TH2F
-// 					> ("response_withoutFakes", TString(
-// 							"response;RECO(" + variable_under_analysis_ + ");GEN(" + variable_under_analysis_ + ")"), variable_n_bins_, variable_min_, variable_max_, variable_n_bins_, variable_min_, variable_max_);
+	response_ =
+			fs->make < TH2F
+					> ("response", TString(
+							"response;RECO(" + variable_under_analysis_ + ");GEN(" + variable_under_analysis_ + ")"), variable_n_bins_, variable_min_, variable_max_, variable_n_bins_, variable_min_, variable_max_);
+	response_without_fakes_ =
+			fs->make < TH2F
+					> ("response_withoutFakes", TString(
+							"response;RECO(" + variable_under_analysis_ + ");GEN(" + variable_under_analysis_ + ")"), variable_n_bins_, variable_min_, variable_max_, variable_n_bins_, variable_min_, variable_max_);
 
 //histograms with asymmetric bins (final measurement)
 	//cout << "But what other options do I have?" << endl;
@@ -158,13 +158,13 @@ void UnfoldingAnalyser::beginJob() {
 							"response;RECO(" + variable_under_analysis_ + ");GEN(" + variable_under_analysis_ + ")"), n_asym_bins, METBinEdges, n_asym_bins, METBinEdges);
 
 	//cout << "Well, you could use 'scram b' as a compiler on your machine within an IDE" << endl;
- 	truth_->Sumw2();
- 	measured_->Sumw2();
- 	fake_->Sumw2();
- 	contamination_in_gen_variable_->Sumw2();
- 	contamination_in_reco_variable_->Sumw2();
-// 	response_->Sumw2();
-// 	response_without_fakes_->Sumw2();
+	truth_->Sumw2();
+	measured_->Sumw2();
+	fake_->Sumw2();
+	contamination_in_gen_variable_->Sumw2();
+	contamination_in_reco_variable_->Sumw2();
+ 	response_->Sumw2();
+ 	response_without_fakes_->Sumw2();
 
 	truth_asym_bins_->Sumw2();
 	measured_asym_bins_->Sumw2();
@@ -196,8 +196,6 @@ void UnfoldingAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetup&
 	double btagWeight(*btagWeightHandle);
 	double weight(puWeight * btagWeight);
 
-	
-
 	bool passes_selection(passesFilter(iEvent, selection_flag_input_));
 	bool is_fully_hadronic(passesFilter(iEvent, is_fully_hadronic_ttbar_flag_));
 	bool is_dileptonic(passesFilter(iEvent, is_dileptonic_ttbar_flag_));
@@ -205,10 +203,9 @@ void UnfoldingAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetup&
 	bool is_semileptonic_electron(passesFilter(iEvent, is_semileptonic_electron_flag_));
 	bool is_semileptonic_muon(passesFilter(iEvent, is_semileptonic_muon_flag_));
 	is_semileptonic_ = is_semileptonic_tau || is_semileptonic_electron || is_semileptonic_muon;
-	
-		
+
 	float gen_variable(get_gen_variable(iEvent));
-		
+
 	if (do_electron_channel_) {
 		if (is_semileptonic_electron) {
 			//PU weight only (no btag-weight) as no b-tagging is applied
@@ -221,11 +218,11 @@ void UnfoldingAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetup&
 
 			measured_->Fill(reco_variable, weight);
 			measured_asym_bins_->Fill(reco_variable, weight);
-			//response_->Fill(reco_variable, gen_variable, weight);
+			response_->Fill(reco_variable, gen_variable, weight);
 			response_asym_bins_->Fill(reco_variable, gen_variable, weight);
 
 			if (is_semileptonic_electron) {
-				//response_without_fakes_->Fill(reco_variable, gen_variable, weight);
+				response_without_fakes_->Fill(reco_variable, gen_variable, weight);
 				response_without_fakes_asym_bins_->Fill(reco_variable, gen_variable, weight);
 			} else {
 				fake_->Fill(reco_variable, weight);
@@ -254,14 +251,14 @@ void UnfoldingAnalyser::analyze(const edm::Event& iEvent, const edm::EventSetup&
 			float reco_variable(get_reco_variable(iEvent));
 
 			weight *= muonCorrection;
-			
+
 			measured_->Fill(reco_variable, weight);
 			measured_asym_bins_->Fill(reco_variable, weight);
-			//response_->Fill(reco_variable, gen_variable, weight);
+			response_->Fill(reco_variable, gen_variable, weight);
 			response_asym_bins_->Fill(reco_variable, gen_variable, weight);
 
 			if (is_semileptonic_muon) {
-				//response_without_fakes_->Fill(reco_variable, gen_variable, weight);
+				response_without_fakes_->Fill(reco_variable, gen_variable, weight);
 				response_without_fakes_asym_bins_->Fill(reco_variable, gen_variable, weight);
 			} else {
 				fake_->Fill(reco_variable, weight);
@@ -297,7 +294,7 @@ float UnfoldingAnalyser::get_gen_variable(const edm::Event& iEvent) const {
 	else if (variable_under_analysis_ == "MT")
 		return get_gen_mt(iEvent);
 	else if (variable_under_analysis_ == "MT_nu")
-		return get_gen_mt_nu(iEvent);	
+		return get_gen_mt_nu(iEvent);
 	else if (variable_under_analysis_ == "WPT")
 		return get_gen_wpt(iEvent);
 	else if (variable_under_analysis_ == "WPT_nu")
@@ -309,34 +306,44 @@ float UnfoldingAnalyser::get_gen_variable(const edm::Event& iEvent) const {
 }
 
 float UnfoldingAnalyser::get_gen_met(const edm::Event& iEvent) const {
-	
+
 	edm::Handle < reco::GenMETCollection > genMETs;
 	iEvent.getByLabel(gen_MET_input_, genMETs);
 	reco::GenMET genMETObject(genMETs->at(0));
-	
+
 	return genMETObject.pt();
 }
 
 float UnfoldingAnalyser::get_gen_met_nu(const edm::Event& iEvent) const {
-	
+
 	if (!is_semileptonic_)
 		return -1.;
-	
+
 	edm::Handle < TtGenEvent > genEvt;
-        iEvent.getByLabel(gen_event_input_, genEvt);
-	
+	iEvent.getByLabel(gen_event_input_, genEvt);
+
 	return genEvt->singleNeutrino()->et();
 }
 
 float UnfoldingAnalyser::get_gen_ht(const edm::Event& iEvent) const {
 	edm::Handle < reco::GenJetCollection > jets;
 	iEvent.getByLabel(gen_jet_input_, jets);
+
+	edm::Handle < TtGenEvent > genEvt;
+	iEvent.getByLabel(gen_event_input_, genEvt);
+	//get lepton
+	const reco::GenParticle* lepton = get_gen_lepton(iEvent);
+
 	float ht(0.);
 
 	//Take ALL the jets!
 	for (unsigned int index = 0; index < jets->size(); ++index) {
-	if(jets->at(index).pt() > 20)
-		ht += jets->at(index).pt();
+		if (jets->at(index).pt() > 20) {
+			double dR = deltaR(*lepton, jets->at(index));
+			if (dR > 0.3)
+				ht += jets->at(index).pt();
+
+		}
 	}
 	return ht;
 }
@@ -347,7 +354,7 @@ float UnfoldingAnalyser::get_gen_st(const edm::Event& iEvent) const {
 	// ST = HT + MET + lepton pt
 	float ht = get_gen_ht(iEvent);
 	float met = get_gen_met(iEvent);
-	
+
 	//get lepton
 	const reco::GenParticle* lepton = get_gen_lepton(iEvent);
 	return ht + met + lepton->pt();
@@ -356,10 +363,17 @@ float UnfoldingAnalyser::get_gen_st(const edm::Event& iEvent) const {
 float UnfoldingAnalyser::get_gen_ht_nocuts(const edm::Event& iEvent) const {
 	edm::Handle < reco::GenJetCollection > jets;
 	iEvent.getByLabel(gen_jet_input_, jets);
+	edm::Handle < TtGenEvent > genEvt;
+	iEvent.getByLabel(gen_event_input_, genEvt);
+	//get lepton
+	const reco::GenParticle* lepton = get_gen_lepton(iEvent);
+
 	float ht(0.);
 
 	//Take ALL the jets!
 	for (unsigned int index = 0; index < jets->size(); ++index) {
+		double dR = deltaR(*lepton, jets->at(index));
+		if (dR > 0.3)
 			ht += jets->at(index).pt();
 	}
 	return ht;
@@ -370,21 +384,22 @@ float UnfoldingAnalyser::get_gen_ht_parton(const edm::Event& iEvent) const {
 		return -1.;
 
 	edm::Handle < TtGenEvent > genEvt;
-    	iEvent.getByLabel(gen_event_input_, genEvt);
-	
+	iEvent.getByLabel(gen_event_input_, genEvt);
+
 	//radiation from leptonic top
 	double leptTopRad = 0;
-	for(unsigned int i = 0; i < genEvt->leptonicDecayTopRadiation().size(); i++){
-	leptTopRad += genEvt->leptonicDecayTopRadiation().at(i)->pt();	
+	for (unsigned int i = 0; i < genEvt->leptonicDecayTopRadiation().size(); i++) {
+		leptTopRad += genEvt->leptonicDecayTopRadiation().at(i)->pt();
 	}
-	
+
 	//radiation from hadronic top
 	double hadTopRad = 0;
-	for(unsigned int i = 0; i < genEvt->hadronicDecayTopRadiation().size(); i++){
-	hadTopRad += genEvt->hadronicDecayTopRadiation().at(i)->pt();	
+	for (unsigned int i = 0; i < genEvt->hadronicDecayTopRadiation().size(); i++) {
+		hadTopRad += genEvt->hadronicDecayTopRadiation().at(i)->pt();
 	}
-	
-	double HT_parton=genEvt->hadronicDecayQuark()->pt()+genEvt->hadronicDecayQuarkBar()->pt()+genEvt->leptonicDecayB()->pt()+genEvt->hadronicDecayB()->pt()+leptTopRad+hadTopRad;
+
+	double HT_parton = genEvt->hadronicDecayQuark()->pt() + genEvt->hadronicDecayQuarkBar()->pt()
+			+ genEvt->leptonicDecayB()->pt() + genEvt->hadronicDecayB()->pt() + leptTopRad + hadTopRad;
 
 	return HT_parton;
 }
@@ -395,7 +410,7 @@ float UnfoldingAnalyser::get_gen_st_nocuts(const edm::Event& iEvent) const {
 	// ST = HT + MET + lepton pt
 	float ht = get_gen_ht_nocuts(iEvent);
 	float met = get_gen_met(iEvent);
-	
+
 	//get lepton
 	const reco::GenParticle* lepton = get_gen_lepton(iEvent);
 	return ht + met + lepton->pt();
@@ -406,17 +421,16 @@ float UnfoldingAnalyser::get_gen_st_parton(const edm::Event& iEvent) const {
 		return -1.;
 
 	edm::Handle < TtGenEvent > genEvt;
-    	iEvent.getByLabel(gen_event_input_, genEvt);
+	iEvent.getByLabel(gen_event_input_, genEvt);
 
 	float ht = get_gen_ht_parton(iEvent);
-	
-	double ST_parton=genEvt->singleLepton()->pt()+genEvt->singleNeutrino()->pt()+ht;
+
+	double ST_parton = genEvt->singleLepton()->pt() + genEvt->singleNeutrino()->pt() + ht;
 
 	return ST_parton;
 }
 
 float UnfoldingAnalyser::get_gen_mt(const edm::Event& iEvent) const {
-
 
 	if (!is_semileptonic_)
 		return -1.;
@@ -432,7 +446,7 @@ float UnfoldingAnalyser::get_gen_mt(const edm::Event& iEvent) const {
 	double momentum_squared = pow(lepton->px() + met.px(), 2) + pow(lepton->py() + met.py(), 2);
 	double MT_squared = energy_squared - momentum_squared;
 //	cout << "MTgen: " << sqrt(MT_squared) << endl;
-	
+
 	if (MT_squared > 0)
 		return sqrt(MT_squared);
 	else
@@ -440,30 +454,28 @@ float UnfoldingAnalyser::get_gen_mt(const edm::Event& iEvent) const {
 }
 
 float UnfoldingAnalyser::get_gen_mt_nu(const edm::Event& iEvent) const {
-    
-    if (!is_semileptonic_)
-	return -1.;
-    
-    edm::Handle < TtGenEvent > genEvt;
-    iEvent.getByLabel(gen_event_input_, genEvt);
+
+	if (!is_semileptonic_)
+		return -1.;
+
+	edm::Handle < TtGenEvent > genEvt;
+	iEvent.getByLabel(gen_event_input_, genEvt);
 
 //need to use lepton and neutrino for this!
-	
+
 	double energy_squared = pow(genEvt->singleLepton()->et() + genEvt->singleNeutrino()->et(), 2);
-	double momentum_squared = pow(genEvt->singleLepton()->px() + genEvt->singleNeutrino()->px(), 2) + pow(genEvt->singleLepton()->py() + genEvt->singleNeutrino()->py(), 2);
+	double momentum_squared = pow(genEvt->singleLepton()->px() + genEvt->singleNeutrino()->px(), 2)
+			+ pow(genEvt->singleLepton()->py() + genEvt->singleNeutrino()->py(), 2);
 	double MT_squared = energy_squared - momentum_squared;
-	
+
 	if (MT_squared > 0)
 		return sqrt(MT_squared);
 	else
 		return -1;
-    
 
 }
 
-
 float UnfoldingAnalyser::get_gen_wpt(const edm::Event& iEvent) const {
-
 
 	if (!is_semileptonic_)
 		return -1.;
@@ -476,17 +488,16 @@ float UnfoldingAnalyser::get_gen_wpt(const edm::Event& iEvent) const {
 	iEvent.getByLabel(gen_MET_input_, genMETs);
 	reco::GenMET met(genMETs->at(0));
 
-	return sqrt(pow(lepton->px()+met.px(),2)+pow(lepton->py()+met.py(),2));
+	return sqrt(pow(lepton->px() + met.px(), 2) + pow(lepton->py() + met.py(), 2));
 }
 
 float UnfoldingAnalyser::get_gen_wpt_nu(const edm::Event& iEvent) const {
 
-
 	if (!is_semileptonic_)
 		return -1.;
-		
-        edm::Handle < TtGenEvent > genEvt;
-    	iEvent.getByLabel(gen_event_input_, genEvt);
+
+	edm::Handle < TtGenEvent > genEvt;
+	iEvent.getByLabel(gen_event_input_, genEvt);
 
 	return genEvt->leptonicDecayW()->pt();
 }
@@ -494,9 +505,11 @@ float UnfoldingAnalyser::get_gen_wpt_nu(const edm::Event& iEvent) const {
 float UnfoldingAnalyser::get_reco_variable(const edm::Event& iEvent) const {
 	if (variable_under_analysis_ == "MET" || variable_under_analysis_ == "MET_nu")
 		return get_reco_met(iEvent);
-	else if (variable_under_analysis_ == "HT" || variable_under_analysis_ == "HT_nocuts" || variable_under_analysis_ == "HT_parton")
+	else if (variable_under_analysis_ == "HT" || variable_under_analysis_ == "HT_nocuts"
+			|| variable_under_analysis_ == "HT_parton")
 		return get_reco_ht(iEvent);
-	else if (variable_under_analysis_ == "ST" || variable_under_analysis_ == "ST_nocuts" || variable_under_analysis_== "ST_parton")
+	else if (variable_under_analysis_ == "ST" || variable_under_analysis_ == "ST_nocuts"
+			|| variable_under_analysis_ == "ST_parton")
 		return get_reco_st(iEvent);
 	else if (variable_under_analysis_ == "MT" || variable_under_analysis_ == "MT_nu")
 		return get_reco_mt(iEvent);
@@ -512,21 +525,23 @@ float UnfoldingAnalyser::get_reco_met(const edm::Event& iEvent) const {
 	edm::Handle < std::vector<pat::MET> > recoMETs;
 	iEvent.getByLabel(reco_MET_input_, recoMETs);
 
-
 	pat::MET recoMETObject(recoMETs->at(0));
-	
+
 	return recoMETObject.pt();
 }
 
 float UnfoldingAnalyser::get_reco_ht(const edm::Event& iEvent) const {
 	edm::Handle < pat::JetCollection > jets;
 	iEvent.getByLabel(reco_jet_input_, jets);
+	edm::Handle < TtGenEvent > genEvt;
+	iEvent.getByLabel(gen_event_input_, genEvt);
+
 	float ht(0.);
-	
+
 	//Take ALL the jets!
 	for (unsigned int index = 0; index < jets->size(); ++index) {
 		const pat::Jet jet = jets->at(index);
-		ht += getSmearedJetPtScale(jets->at(index), 0)*jets->at(index).pt();
+		ht += getSmearedJetPtScale(jets->at(index), 0) * jets->at(index).pt();
 	}
 	return ht;
 }
@@ -548,10 +563,9 @@ float UnfoldingAnalyser::get_reco_mt(const edm::Event& iEvent) const {
 	iEvent.getByLabel(reco_MET_input_, recoMETs);
 
 	pat::MET met(recoMETs->at(0));
-	
-        //cout << "RECO lep py: " <<  lepton->py() << " , met px: " << met.px() << endl;
-	
-	
+
+	//cout << "RECO lep py: " <<  lepton->py() << " , met px: " << met.px() << endl;
+
 	//combine their x & y momenta to get the transverse W boson mass
 	double energy_squared = pow(lepton->et() + met.et(), 2);
 	double momentum_squared = pow(lepton->px() + met.px(), 2) + pow(lepton->py() + met.py(), 2);
@@ -571,8 +585,8 @@ float UnfoldingAnalyser::get_reco_wpt(const edm::Event& iEvent) const {
 	iEvent.getByLabel(reco_MET_input_, recoMETs);
 
 	pat::MET met(recoMETs->at(0));
-	
-	return sqrt(pow(lepton->px()+met.px(),2)+pow(lepton->py()+met.py(),2)); 
+
+	return sqrt(pow(lepton->px() + met.px(), 2) + pow(lepton->py() + met.py(), 2));
 }
 
 const reco::GenParticle* UnfoldingAnalyser::get_gen_lepton(const edm::Event& iEvent) const {
@@ -597,22 +611,22 @@ const reco::Candidate* UnfoldingAnalyser::get_reco_lepton(const edm::Event& iEve
 
 float UnfoldingAnalyser::get_muon_correction(const edm::Event& iEvent) const {
 	//ID, iso and trigger correction respectively from:  https://indico.cern.ch/getFile.py/access?contribId=3&resId=0&materialId=slides&confId=214870
-	
+
 	double correction(1.);
-	
+
 	edm::Handle < pat::MuonCollection > signalMuon;
 	iEvent.getByLabel(muon_input_, signalMuon);
-	
+
 	double muEta = signalMuon->at(0).eta();
 
-	if(abs(muEta)<0.9)
-		correction = 0.9941*0.9923*0.9560;
-	else if(abs(muEta)>=0.9 && abs(muEta)<1.2)
-		correction = 0.9917*0.9979*0.9528;
-	else if(abs(muEta)>=1.2)
-		correction = 0.9982*1.0019*0.9809;
-	
-	return correction; 
+	if (abs(muEta) < 0.9)
+		correction = 0.9941 * 0.9923 * 0.9560;
+	else if (abs(muEta) >= 0.9 && abs(muEta) < 1.2)
+		correction = 0.9917 * 0.9979 * 0.9528;
+	else if (abs(muEta) >= 1.2)
+		correction = 0.9982 * 1.0019 * 0.9809;
+
+	return correction;
 }
 
 DEFINE_FWK_MODULE (UnfoldingAnalyser);
