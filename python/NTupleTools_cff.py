5ec70c8755b47f734ddb4819c2c6787098d19810
Luke Kreczko <lkreczko@gmail.com>
2014-04-08 14:27:10 +0100

    backports from master

--- python/NTupleTools_cff.py
+++ python/NTupleTools_cff.py
@@ -1,15 +1,27 @@
 # Starting with a skeleton process which gets imported with the following line
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
+# Import LumiList for running on specific JSON files or excluding/including specific events when running locally.
+import FWCore.PythonUtilities.LumiList as LumiList
+
 from PhysicsTools.PatAlgos.tools.coreTools import *
 #set up analysis
 #https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideFrontierConditions
-GLOBALTAG_DATA = 'FT_R_53_V6C::All' #2012 A and B: data re-reco with CMSSW_5_3_3_patch1
-# GLOBALTAG_DATA = 'FT_R_53_V10:All' #2012 C v1: data re-reco with CMSSW_5_3_2_patch4
-GLOBALTAG_MC = 'START53_V11::All'
+
+#Globals tags have been updated with the new JEC from https://twiki.cern.ch/twiki/bin/viewauth/CMS/JECDataMC
+#Data Global Tag
+GLOBALTAG_DATA = 'FT_53_V21_AN3::All' # Used for 2012 A, B, C and D: 22Jan2013 re-reco
+#GLOBALTAG_DATA = 'FT53_V10A_AN4::All' # Used for 2012 C: 24Aug2012 re-reco (v1)
+#GLOBALTAG_DATA = 'GR_P_V42_AN4::All' # Used for 2012 C prompt reco (v2) and Run 2012 D prompt reco (v1)
+#GLOBALTAG_DATA = 'FT_P_V42C_AN4::All' # Used for 2012 C run 201191: ecal recovery of run 201191 (11Dec2012 re-reco)
+
+#Monte Carlo Global Tag
+GLOBALTAG_MC = 'START53_V21::All'
 FILETAG = '53X'
-TEST_DATA_FILE = 'file:///storage/TopQuarkGroup/test/SingleElectron_Run2012B_196531_524_PromptReco-v1_AOD.root'
+
+TEST_DATA_FILE = 'file:///storage/TopQuarkGroup/test/SingleElectron_Run2012B_13Jul2012_ReReco_AOD.root'
 TEST_MC_FILE = 'file:///storage/TopQuarkGroup/mc/8TeV/SynchEx/Summer12_DR53X_TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_AODSIM_PU_S10_START53_V7A-v1.root'
+
 #CERN
 #TEST_DATA_FILE = '/store/data/Run2012A/ElectronHad/AOD/PromptReco-v1/000/193/336/C47F154E-A697-E111-83F5-001D09F24D8A.root'
 #TEST_MC_FILE = 'file:///afs/cern.ch/user/s/senkin/workspace/public/Summer12_DR53X_TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_AODSIM_PU_S10_START53_V7A-v1.root'
@@ -19,6 +31,9 @@ TEST_MC_FILE = 'file:///storage/TopQuarkGroup/mc/8TeV/SynchEx/Summer12_DR53X_TTJ
 USE_JEC_FROM_DB = False
 #if 'False' taus are included in the jet collections
 removeTausFromJetCollection = False
+#if remove eles from gen jets
+excludeElectronsFromWsFromGenJets = True	
+
 #maximal relative isolation for loose leptons
 maxLooseLeptonRelIso = 999.0
 
@@ -60,13 +75,13 @@ options.register ('setupMETmanually',
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.bool,
                   "Alternative way of setting up PFMET with PF2PAT (see python/MET_Setup_cff.py)")
- 
+
 options.register ('applyType0METcorrection',
                   False,
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.bool,
                   "Apply type0 MET correction")
- 
+
 options.register ('applySysShiftCorrection',
                   False,
                   VarParsing.multiplicity.singleton,
@@ -110,7 +125,7 @@ options.register ('printEventContent',
                   "Outputs the event content at the end of the path")
 
 options.register ('CMSSW',
-                  '44X',
+                  '53X',
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.string,
                   "CMSSW version used: 53X (default), 52X or 44X")
@@ -131,13 +146,16 @@ options.register ('isMCatNLO',
                   False,
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.bool,
-                  "Identify if samples contains ttbar MC@NLO events (different genParticle structure)")
+		 "Identify if samples contains ttbar MC@NLO events (different genParticle structure)")
 
 options.register ('skipEvents',
-                              0,
-                           VarParsing.multiplicity.singleton,
-                           VarParsing.varType.int,
-                               "Number of events to skip (0 for none)")
+                  0,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.int,
+                  "Number of events to skip (0 for none)")
+
+			       
+			       
 #CMSSW 44X can't compile this file in the python directory correctly 
 #as it fails to identify the file ending. This hack helps.
 import sys
@@ -191,6 +209,11 @@ else:
     process.source.fileNames = [
             TEST_DATA_FILE
             ]
+
+# Use lumisToProcess to specify a JSON file to use when running locally.
+#process.source.lumisToProcess = LumiList.LumiList(filename = 'BristolAnalysis/NTupleTools/data/CertifiedJSONs/Cert_190456-208686_8TeV_22Jan2013ReReco_Collisions12_JSON.txt').getVLuminosityBlockRange()
+# Use eventsToSkip to specify events which you do not want to process when running locally.
+#process.source.eventsToSkip = cms.untracked.VEventRange('193336:280110630')
     
 # add the flavor history
 process.load("PhysicsTools.HepMCCandAlgos.flavorHistoryPaths_cfi")
@@ -224,15 +247,15 @@ process.EventFilters = setup_eventfilters(process, cms, options)
 from BristolAnalysis.NTupleTools.PF2PAT_Setup_cff import *
 setup_PF2PAT(process, cms, options, postfix=postfix, removeTausFromJetCollection=removeTausFromJetCollection)
 setup_looseLeptons(process, cms, options, postfix=postfix, maxLooseLeptonRelIso=maxLooseLeptonRelIso)
+#Jets
+from BristolAnalysis.NTupleTools.Jets_Setup_cff import *
+setup_jets(process, cms, options, postfix=postfix)
 #MET
 from BristolAnalysis.NTupleTools.MET_Setup_cff import *
 if options.setupMETmanually:
     setup_MET_manually(process, cms, options, postfix=postfix)
 else:
     setup_MET(process, cms, options, postfix=postfix)
-#Jets
-from BristolAnalysis.NTupleTools.Jets_Setup_cff import *
-setup_jets(process, cms, options, postfix=postfix)
 #electron ID
 from BristolAnalysis.NTupleTools.ElectronID_cff import *
 setup_electronID(process, cms)
@@ -249,11 +272,20 @@ process.patElectronsLoosePFlow.pfElectronSource = cms.InputTag("pfIsolatedElectr
 if options.useData :
     removeMCMatching(process, ['All'])
     process.looseLeptonSequence.remove(process.muonMatchLoosePFlow)
+    
+if not options.useData :
+    ## needed for redoing the ak5GenJets
+    process.load("BristolAnalysis.NTupleTools.GenJetParticles_cfi")
+    process.load("RecoJets.Configuration.RecoGenJets_cff")
+    #exclude the following particles with ids below from the gen jets electron had to be added, the rest are default
+    if excludeElectronsFromWsFromGenJets :
+        process.genParticlesForJetsNoNu.excludeFromResonancePids = [11, 12, 13, 14, 16]
 
 process.patseq = cms.Sequence(
-    process.HBHENoiseFilterResultProducer * 
+    process.HBHENoiseFilter * 
     process.goodOfflinePrimaryVertices * 
-    process.genParticlesForJetsNoNu * 
+    process.eidMVASequence *
+    process.genParticlesForJetsNoNu *
     getattr(process, "patPF2PATSequence" + postfix) * 
     process.looseLeptonSequence * 
     process.patDefaultSequence * 
@@ -264,15 +296,12 @@ process.patseq = cms.Sequence(
     process.flavorHistorySeq 
     )
 
-process.patseq.replace(process.goodOfflinePrimaryVertices,
-                            process.goodOfflinePrimaryVertices * 
-                            process.eidCiCSequence)
-
 if options.useData:
     process.patseq.remove(process.genParticlesForJetsNoNu)
     process.patseq.remove(process.genJetParticles)    
     process.patseq.remove(process.flavorHistorySeq)
-
+    process.patseq.remove(process.genParticlesForJets)
+        
 # remove flavour history as it has problems with the MC@NLO genparticles
 if options.isMCatNLO:
     process.patseq.remove(process.flavorHistorySeq)
@@ -326,6 +355,21 @@ else :
 # reduce verbosity
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
 
+#----------------------------------------------------------------------------------------------------
+# The ECAL laser correction filter (ecalLaserCorrFilter) occasionally 
+# interpolates laser correction values of < 1 and issues a LogError message
+# 
+# This does not affect the laser correction that is applied: only the interpolated estimate 
+# that the filter uses.  The filter runs in "Tagging Mode", so no events can be removed.
+# 
+# Error message comes from line 173 of:
+# CalibCalorimetry/EcalLaserCorrection/src/EcalLaserDbService.cc
+# function = EcalLaserDbService::getLaserCorrection
+# Message = "The interpolated laser correction is <= zero!"
+#
+# We suppress these messages.  Suppression can be removed by commenting the following line.
+#----------------------------------------------------------------------------------------------------
+process.MessageLogger.suppressError = cms.untracked.vstring ('ecalLaserCorrFilter') 
 
 # process all the events
 if options.maxEvents:
