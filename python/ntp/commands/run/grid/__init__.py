37ca43c9319353cad44f0f5f1dbe02deb8194f6e
Lukasz Kreczko <lkreczko@googlemail.com>
2016-05-11 11:30:21 +0100

    added run grid documentation and skeleton for check command

--- python/ntp/commands/run/grid/__init__.py
+++ python/ntp/commands/run/grid/__init__.py
@@ -1,6 +1,19 @@
 """
     run grid:    Uses CRAB3 to submit jobs to the WLCG grid
+        Usage:
+            run grid [campaign=<X>]  [dataset=<X>] [skip=<X>]
+
+        Parameters:
+            campaign: which campaign to run. Corresponds to the folder
+                      structure in crab/*
+            dataset:  Alias for the dataset you want to run over. Corresponds
+                      to the file names (without extension) in crab/*/*.py.
+                      Accepts wild-cards and comma-separated lists.
+                      Default is to run over all datasets for a given campaign.
+            skip:     Alias for datasets you want to skip.
+                      Accepts wild-cards and comma-separated lists.
 """
+
 from __future__ import print_function
 from .. import Command as C
 
