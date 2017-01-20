f9e778a5283d0ce2037df137b1deced030b7eb2a
kreczko <lkreczko@googlemail.com>
2017-01-20 16:38:00 +0000

    reduced files per job for ttz due to larger dataset

--- python/crab/Moriond17/TTZToLLNUNU_M10.py
+++ python/crab/Moriond17/TTZToLLNUNU_M10.py
@@ -10,6 +10,5 @@ config.Data.outputDatasetTag = NAME
 config.Data.outLFNDirBase += '/' + CAMPAIGN
 config.Data.inputDataset = '/TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 5
 config.JobType.pyCfgParams = ['isTTbarMC=1']
-
